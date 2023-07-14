957c8e7c9b06cf136faf433e598a205f077992d1
JonatasMSS <jonatas.miguelss@gmail.com>
2023-07-14 14:01:30 -0300

    removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-5">
+      <div className="w-full rounded-lg border-2 border-black p-2">
+        <span className="font-bold">Filtrar por</span>
+      </div>
+    </main>
   );
 }
