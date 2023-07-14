9eb6a8b622968f734bd60ee8dcf0df60abedd753
JonatasMSS <jonatas.miguelss@gmail.com>
2023-07-13 22:32:19 -0300

    Added a Inside Component

--- src/components/UserInfoSide.tsx
+++ src/components/UserInfoSide.tsx
@@ -4,12 +4,25 @@ import { Header } from "./Header";
 import { BigIntroFlashcard } from "./BigIntroFlashcard";
 
 export function UserInfoSide() {
+  function MetricSection() {
+    return (
+      <div className="flex w-full flex-col p-2">
+        <span className="w-full border-b-2 border-zinc-700 text-2xl font-bold">
+          Suas métricas
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div className="flex h-full w-full flex-col items-center border-r-2 border-zinc-800 bg-sahara ">
       {/* Header */}
       <Header />
 
       <BigIntroFlashcard />
+
+      {/* Data Section */}
+      <MetricSection />
     </div>
   );
 }
