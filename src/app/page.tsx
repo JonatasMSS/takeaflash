b31df93f1eb798bc2409345bc6cc856be505c07d
JonatasMSS <jonatas.miguelss@gmail.com>
2023-07-13 22:33:07 -0300

    [removed]: unused sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,5 @@
-import { prisma } from '@/lib/prisma'
-import { GetServerSideProps } from 'next'
-import Image from 'next/image'
-
-async function getData() {
-  const tags = await prisma.tag.findMany()
-
-  return tags
-}
-
-
 export default async function Home() {
-
-  const tags = await getData()
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>
-        {JSON.stringify(tags)}
-      </p>
-    </main>
-  )
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+  );
 }
-
-
-
-
