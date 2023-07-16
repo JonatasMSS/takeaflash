68890d0a2822b016533b1d59371429c88c9d0aaf
JonatasMSS <jonatas.miguelss@gmail.com>
2023-07-16 15:47:19 -0300

    [remove-config]:removed unescessary content

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env:{
-        GOOGLE_CLIENT_ID:process.env.GOOGLE_CLIENT_ID,
-        GOOGLE_CLIENT_SECRET:process.env.GOOGLE_CLIENT_SECRET,
-        AUTH_URL:process.env.AUTH_URL
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
