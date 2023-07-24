4907d2d6f8220a46cd833f30e255f0824cc47cee
JonatasMSS <jonatas.miguelss@gmail.com>
2023-07-23 22:21:08 -0300

    [feat]:validator created

--- src/components/FormDialog/Form.tsx
+++ src/components/FormDialog/Form.tsx
@@ -1,14 +1,62 @@
 "use client";
-
+import * as Dialog from "@radix-ui/react-dialog";
 import { Session } from "next-auth";
 import { SelectorItem } from "../Selector";
 import { ChangeEvent, useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { ErrorShow } from "./ErrorShow";
+import { useLocalStorage } from "@/lib/LocalStorage";
 
 interface FormProps {}
 
-export function Form({ tags }: { tags: SelectorItem }) {
+export function Form({
+  tags,
+  session,
+}: {
+  tags: SelectorItem;
+  session: Session | null;
+}) {
+  const { setLocalFlashcards, setLocalTags } = useLocalStorage();
+
+  const validationSchema = yup.object({
+    tag: yup.string().required("Tag requerida!"),
+    color: yup.string(),
+    title: yup.string().required("Title requerida!"),
+    content: yup.string().required("Content requerido!"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      tag: "",
+      color: "#000000",
+      title: "",
+      content: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: ({ title, color, tag, content }) => {
+      setLocalTags([
+        {
+          tagColor: color,
+          text: tag,
+          value: tag.toLocaleLowerCase(),
+        },
+      ]);
+      setLocalFlashcards({
+        title,
+        content,
+        tag,
+        tagColor: color,
+      });
+      alert("Flashcard criado com sucesso!");
+    },
+  });
+
   return (
-    <form className="my-1 flex w-full flex-col gap-4">
+    <form
+      onSubmit={formik.handleSubmit}
+      className="my-1 flex w-full flex-col gap-4"
+    >
       {/* Tag and title */}
       <div className="flex w-full items-center gap-5">
         {/* Tag and color */}
@@ -18,18 +66,25 @@ export function Form({ tags }: { tags: SelectorItem }) {
             <input
               className="rounded-md p-1 text-start outline-none transition-all  focus:border-2 focus:border-black"
               list="tags"
-              name="tag"
+              {...formik.getFieldProps("tag")}
               id="tag"
             />
-
             <datalist id="tags">
               {tags.map((tag, i) => (
                 <option key={i} value={tag.value} />
               ))}
             </datalist>
 
-            <input type="color" className="rounded-md" />
+            <input
+              type="color"
+              id="color"
+              {...formik.getFieldProps("color")}
+              className="rounded-md"
+            />
           </div>
+          {formik.touched.tag && formik.errors.tag && (
+            <ErrorShow text={formik.errors.tag} />
+          )}
         </div>
 
         {/*Title*/}
@@ -38,9 +93,12 @@ export function Form({ tags }: { tags: SelectorItem }) {
           <input
             type="text"
             className="rounded-md p-1 outline-none transition-all focus:border-2 focus:border-black"
-            name="title"
+            {...formik.getFieldProps("title")}
             id="title"
           />
+          {formik.touched.title && formik.errors.title && (
+            <ErrorShow text={formik.errors.title} />
+          )}
         </div>
       </div>
 
@@ -50,20 +108,30 @@ export function Form({ tags }: { tags: SelectorItem }) {
         <input
           type="text"
           className="rounded-md p-1 outline-none transition-all focus:border-2 focus:border-black"
-          name="content"
+          {...formik.getFieldProps("content")}
           id="content"
         />
+        {formik.touched.content && formik.errors.content && (
+          <ErrorShow text={formik.errors.content} />
+        )}
       </div>
 
       {/* Action buttons */}
       <div className="flex w-full gap-2">
-        <button className="my-2 rounded-md bg-green-400 p-1 transition-all hover:bg-green-300">
+        <button
+          type="submit"
+          className="my-2 rounded-md bg-green-400 p-1 transition-all hover:bg-green-300"
+        >
           Confirmar
         </button>
-
-        <button className="my-2 rounded-md bg-red-400 p-1 transition-all hover:bg-red-300">
-          Cancelar
-        </button>
+        <Dialog.Close asChild>
+          <button
+            type="button"
+            className="my-2 rounded-md bg-red-400 p-1 transition-all hover:bg-red-300"
+          >
+            Cancelar
+          </button>
+        </Dialog.Close>
       </div>
     </form>
   );
